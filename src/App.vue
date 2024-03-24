3865de15088dcef145d6636ecfc5b4eaaa2d8312
mane___ <manesargsyan98@gmail.com>
2024-03-24 23:26:17 +0400

    added styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 import { ref } from 'vue'
 
 let count = ref(0)
@@ -23,16 +23,16 @@ function resetCount() {
 </script>
 
 <template>
-  <div>
-    <div>{{ count }}</div>
+  <div class="counterBlock">
+    <div class="count">{{ count }}</div>
     <div>
       <button @click="dectrementByTen()">-10</button>
-      <button @click="decrement()">-</button>
-      <button @click="increment()">+</button>
+      <button class="minus" @click="decrement()">-</button>
+      <button class="plus" @click="increment()">+</button>
       <button @click="incrementByTen">+10</button>
     </div>
     <div>
-      <button @click="resetCount">Reset</button>
+      <button class="reset" @click="resetCount">Reset</button>
     </div>
   </div>
 
@@ -44,12 +44,43 @@ function resetCount() {
     </div>
   </header>
 
-  <main>
+  <!-- <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
 <style scoped>
+.counterBlock {
+  text-align: center;
+  margin-bottom: 30px;
+}
+.count {
+  color: aliceblue;
+  font-size: 25px;
+}
+button {
+  cursor: pointer;
+  color: #989898;
+  font-size: 20px;
+  padding: 5px;
+  margin: 5px;
+  border: 1px solid hsla(160, 100%, 37%, 1);
+  border-radius: 10px;
+  background-color: transparent;
+}
+
+.minus {
+  padding: 5px 12px;
+}
+.plus {
+  padding: 5px 10px;
+}
+
+button:hover {
+  background-color: hsla(160, 100%, 37%, 1);
+  color: aliceblue;
+}
+
 header {
   line-height: 1.5;
 }

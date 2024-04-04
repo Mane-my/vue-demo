eafc345062b0d2feb1620ede3802bf2977f55bd4
mane___ <manesargsyan98@gmail.com>
2024-04-05 01:23:10 +0400

    added exchange currancy converter, deleted vscode folder

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,17 @@
 <script>
+import Exchange from './components/Exchange.vue'
+
 export default {
-  data() {
-    return {
-      txtValid: '',
-      txtInvalid: ''
-    }
+  components: {
+    Exchange
   },
-
-  methods: {
-    promptField(value) {
-      value = prompt('Verify email')
-      console.log(value)
-
-      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)) {
-        this.txtValid = value
-        this.txtInvalid = ''
-      } else {
-        this.txtInvalid = value
-        this.txtValid = ''
-      }
-    },
-
-    resetValues() {
-      this.txtValid = ''
-      this.txtInvalid = ''
-    }
-  }
+  data() {
+    return {}
+  },
+  methods: {}
 }
 </script>
 
 <template>
-  <header>
-    <div class="btns">
-      <button @click="promptField(value)">Input email</button>
-      <button @click="resetValues()">Reset</button>
-    </div>
-
-    <div class="validField" v-if="txtValid">
-      <h1>{{ txtValid }}</h1>
-      <h4>Email is valid</h4>
-    </div>
-
-    <div class="invalidField" v-if="txtInvalid">
-      <h1>{{ txtInvalid }}</h1>
-      <h4>Email is invalid</h4>
-    </div>
-  </header>
+  <Exchange />
 </template>
-
-<style scoped>
-.btns button {
-  cursor: pointer;
-  color: #989898;
-  font-size: 20px;
-  padding: 10px;
-  margin: 5px;
-  border: 1px solid hsla(160, 100%, 37%, 1);
-  border-radius: 10px;
-  background-color: transparent;
-}
-
-.btns button:hover {
-  background-color: hsla(160, 100%, 37%, 1);
-  color: aliceblue;
-}
-
-.validField h1,
-.invalidField h1 {
-  border-radius: 10px;
-  color: aliceblue;
-  text-align: center;
-  margin-top: 20px;
-  padding-bottom: 7px;
-}
-
-.validField h1 {
-  border: 1px solid green;
-}
-
-.validField h4 {
-  color: green;
-}
-
-.invalidField h1 {
-  border: 1px solid red;
-}
-
-.invalidField h4 {
-  color: red;
-}
-</style>
---
+++ src/components/Exchange.vue
@@ -0,0 +1,76 @@
+<script>
+export default {
+  data() {
+    return {
+      value: 0,
+      rate: 400,
+      currency: 'AMD'
+    }
+  },
+
+  methods: {
+    inputFunc(event) {
+      this.value = event.target.value
+    },
+    focusFunc() {
+      if (+this.value === 0) {
+        this.value = ''
+      }
+    },
+    changeCurrency() {
+      if (this.currency === 'AMD') {
+        this.currency = 'USD'
+      } else {
+        this.currency = 'AMD'
+      }
+      this.value = 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="inputDiv" v-if="currency === 'AMD'">
+    <div>
+      <div>AMD -> USD</div>
+      <input @input="inputFunc" @focus="focusFunc" type="number" :value="value" />
+      <input type="number" disabled :value="value / rate" />
+    </div>
+    <button @click="changeCurrency">USD -> AMD</button>
+  </div>
+
+  <div class="inputDiv" v-else>
+    <div>
+      <div>USD -> AMD</div>
+      <input @input="inputFunc" @focus="focusFunc" type="number" :value="value" />
+      <input type="number" disabled :value="value * rate" />
+    </div>
+    <button @click="changeCurrency">AMD -> USD</button>
+  </div>
+</template>
+
+<style scoped>
+.inputDiv input {
+  color: aliceblue;
+  margin: 5px;
+  padding: 5px;
+  background: none;
+  border: 1px solid hsla(160, 100%, 37%, 1);
+  border-radius: 10px;
+}
+
+.inputDiv button {
+  cursor: pointer;
+  color: #989898;
+  padding: 10px;
+  margin: 5px;
+  border: 1px solid hsla(160, 100%, 37%, 1);
+  border-radius: 10px;
+  background-color: transparent;
+}
+
+.inputDiv button:hover {
+  background-color: hsla(160, 100%, 37%, 1);
+  color: aliceblue;
+}
+</style>
